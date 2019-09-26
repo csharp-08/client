5a7c92b673e4a45f61b7690b864b98fcff193655
Victor Paltz <paltz.victor@free.fr>
2019-09-26 17:31:26 +0200

    size

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container" ref='container'>
-    Canvas. Bonjour {{username}} !
+    <div>
+      <div>Canvas. Bonjour {{username}} !</div>
+      <button @click="color = 'red'">Rouge</button>
+      <button @click="color = 'blue'">Bleu</button>
+      <button @click="color = 'green'">Vert</button>
+      <button @click="color = 'white'">Effacer</button>
+      <input @change="updateStroke($event)" type="range"
+          min="3" max="100" value="15" class="slider" id="myRange">
+    </div>
     <v-stage :config="configKonva"
              @mousedown="newLine($event)"
              @mousemove="addPoints($event)"
@@ -35,17 +43,20 @@ export default {
       },
       lineList: [],
       isDrawing: false,
+      color: 'blue',
+      strokeWidth: 15,
     };
   },
   methods: {
     newLine(event) {
       this.lineList.push({
         points: [event.evt.offsetX, event.evt.offsetY, event.evt.offsetX, event.evt.offsetY],
-        stroke: 'blue',
-        strokeWidth: 15,
+        stroke: this.color,
+        strokeWidth: this.strokeWidth,
         lineCap: 'round',
         lineJoin: 'round',
       });
+      console.log('is drawing');
       this.isDrawing = true;
     },
     addPoints(event) {
@@ -57,7 +68,12 @@ export default {
 
     move() {
       console.log('okok');
-    }
+    },
+
+    updateStroke(event) {
+      console.log(event.srcElement.value);
+      this.strokeWidth = event.srcElement.value;
+    },
   },
 };
 </script>
