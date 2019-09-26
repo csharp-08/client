f8da77f2b9e618430bb9dde42f56e142c9e29ed6
Victor Paltz <paltz.victor@free.fr>
2019-09-26 16:45:46 +0200

    drawable canvas

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="container">
+  <div class="container" ref='container'>
     Canvas. Bonjour {{username}} !
+    <v-stage :config="configKonva"
+             @mousedown="newLine($event)"
+             @mousemove="addPoints($event)"
+             @mouseup="isDrawing = false">
+      <v-layer>
+        <li v-for="(item, index) in lineList" :key="`${index}_${item.points.length}`">
+          <v-line :config="item" @mousedown="move"></v-line>
+        </li>
+      </v-layer>
+    </v-stage>
   </div>
 </template>
 
@@ -12,6 +22,42 @@ export default {
       type: String,
       default: '',
     },
+  },
+  mounted() {
+    this.configKonva.width = this.$refs.container.clientWidth;
+    this.configKonva.height = this.$refs.container.clientHeight - 50;
+  },
+  data() {
+    return {
+      configKonva: {
+        width: 0,
+        height: 0,
+      },
+      lineList: [],
+      isDrawing: false,
+    };
+  },
+  methods: {
+    newLine(event) {
+      this.lineList.push({
+        points: [event.evt.offsetX, event.evt.offsetY, event.evt.offsetX, event.evt.offsetY],
+        stroke: 'blue',
+        strokeWidth: 15,
+        lineCap: 'round',
+        lineJoin: 'round',
+      });
+      this.isDrawing = true;
+    },
+    addPoints(event) {
+      if (this.isDrawing) {
+        const lastIndex = this.lineList.length - 1;
+        this.lineList[lastIndex].points.push(event.evt.offsetX, event.evt.offsetY);
+      }
+    },
+
+    move() {
+      console.log('okok');
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import VueKonva from 'vue-konva';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueKonva);
 
 new Vue({
   render: h => h(App),
