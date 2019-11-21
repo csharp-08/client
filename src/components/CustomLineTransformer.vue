70b959bba5d040ca441b75763f869e8faf816e6d
Victor Paltz <paltz.victor@free.fr>
2019-11-21 16:46:46 +0100

    final commit for transform polygon and lines

--- src/components/CustomLineTransformer.vue
+++ src/components/CustomLineTransformer.vue
@@ -2,7 +2,7 @@
   <div>
     <v-line :config="lineConfig"></v-line>
     <template v-for="(x, index) in lineConfig.points">
-      <v-rect :config="{...squareConfig, x: x, y: lineConfig.points[index + 1]}"
+      <v-rect :config="{...squareConfig, x: x-5, y: lineConfig.points[index + 1]-5}"
             v-if="(index%2 === 0) && !(index === 0 && lineConfig.points.length > 4)"
             @dragmove="updatePoint($event, index)"
             @dragend="update"
@@ -50,15 +50,10 @@ export default {
     init() {
       const { x: offsetX, y: offsetY } = this.lineNode.attrs;
       const points = JSON.parse(JSON.stringify(this.lineNode.attrs.points));
-      /* if (x || y) {
-        for (let i = 0; i < points.length; i += 2) {
-          points[i] += x;
-          points[i + 1] += y;
-      */
       if (offsetX || offsetY) { // Removes offset
         for (let i = 0; i < points.length; i += 2) {
-          points[i] += offsetX + 5;
-          points[i + 1] += offsetY + 5;
+          points[i] += offsetX;
+          points[i + 1] += offsetY;
         }
       }
       this.lineConfig.points = points;
@@ -68,19 +63,17 @@ export default {
       if (!event || !event.target) {
         return;
       }
-      console.log(index);
-      console.log(this.lineConfig.points.length);
 
       const { x, y } = event.target.attrs;
       this.lineConfig.points[0 + index] = x + 5;
       this.lineConfig.points[1 + index] = y + 5;
 
       const { x: offsetX, y: offsetY } = this.lineNode.attrs;
-      this.lineNode.attrs.points[0 + index] = x - offsetX;
-      this.lineNode.attrs.points[1 + index] = y - offsetY;
+      this.lineNode.attrs.points[0 + index] = x - offsetX + 5;
+      this.lineNode.attrs.points[1 + index] = y - offsetY + 5;
 
+      // For a polygon we update 2 points
       if (index === this.lineConfig.points.length - 2 && this.lineConfig.points.length > 4) {
-        console.log(this.lineConfig.points.length);
         this.updatePoint(event, 0);
       }
     },
